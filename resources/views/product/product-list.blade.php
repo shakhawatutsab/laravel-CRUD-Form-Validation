e1b42e71d9ad0e5adcae74bcada77458e59f085d
utsab <shakhawatutsab@gmail.com>
2025-07-05 21:42:38 +0600

    Create some blade file & using bootstrap 5^3

---
+++ resources/views/product/product-list.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.layout')
+
+@section('content')
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                Product List
+            </div>
+        </div>
+    </div>
+@endsection

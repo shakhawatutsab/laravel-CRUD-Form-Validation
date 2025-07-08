fee8bbe911cd118ccd5aca43b99aa373ee356936
utsab <shakhawatutsab@gmail.com>
2025-07-09 00:29:34 +0600

    link with product page

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -16,7 +16,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/products">Products</a>
+                <a class="nav-link active" aria-current="page" href="{{ route("product.index") }}">Products</a>
                 </li>
                 <li class="nav-item">
                 <a class="nav-link" href="/categories">Categories</a>
